6882699976b707a51e247a6eb556bcff8609b9fe
sinyawskiy <sinyawskiy@gmail.com>
2018-04-15 19:56:34 +0300

    Update models.py
    deprecated change location of generic foreign key

--- tagging/models.py
+++ tagging/models.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals, absolute_import
 Models and managers for generic tagging.
 """
 
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import connection, models
 from django.utils.translation import ugettext_lazy as _
@@ -507,7 +507,7 @@ class TaggedItem(models.Model):
     tag          = models.ForeignKey(Tag, verbose_name=_('tag'), related_name='items')
     content_type = models.ForeignKey(ContentType, verbose_name=_('content type'))
     object_id    = models.PositiveIntegerField(_('object id'), db_index=True)
-    object       = generic.GenericForeignKey('content_type', 'object_id')
+    object       = GenericForeignKey('content_type', 'object_id')
 
     objects = TaggedItemManager()
 
