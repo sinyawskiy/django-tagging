87a50a78d74fc2cf523480510a86f44d1cf266ab
sinyawskiy <sinyawskiy@gmail.com>
2018-04-16 13:17:43 +0300

    Update urls.py
    django 2 python 3

--- tagging/urls.py
+++ tagging/urls.py
@@ -1,9 +1,10 @@
 #coding: utf-8
 from __future__ import unicode_literals, absolute_import
+from tagging.views import tag_it_suggest, typeahead_suggest
+from django.conf.urls import url
 
-from django.conf.urls import *
-
-urlpatterns = patterns('tagging.views',
-    url(r'^tagit-suggest/$', 'tag_it_suggest', name='tagit-suggest'),
-    url(r'^typeahead-suggest/$', 'typeahead_suggest', name='typeahead-suggest'),
-)
+app_name = 'tagging'
+urlpatterns = [
+    url(r'^tagit-suggest/$', tag_it_suggest, name='tagit-suggest'),
+    url(r'^typeahead-suggest/$', typeahead_suggest, name='typeahead-suggest'),
+]
