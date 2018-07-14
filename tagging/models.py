44096e1208b3f41c975c74ba8dcb792b90af42ea
sinyawskiy <sinyawskiy@gmail.com>
2018-07-14 09:49:49 +0300

    Update models.py
    add import

--- tagging/models.py
+++ tagging/models.py
@@ -1,5 +1,6 @@
 #coding: utf-8
 from __future__ import unicode_literals, absolute_import
+from django.utils.encoding import python_2_unicode_compatible
 """
 Models and managers for generic tagging.
 """
